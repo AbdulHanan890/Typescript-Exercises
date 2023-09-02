bbc5bd5b1a68ff238ab7c29357e219b6944ca9f0
AbdulHanan890 <143878161+AbdulHanan890@users.noreply.github.com>
2023-09-02 06:23:35 -0700

    modified code

---
+++ Exercise_21.ts
@@ -0,0 +1,23 @@
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. 
+//Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.//
+
+const detectRange = (arr:number[], start:number, end:number) => {
+    let result:number[];
+    if(result = arr.slice(start,end)){ 
+        return result;
+    } else if(Error){
+        if(Error instanceof RangeError){
+            return `Range error: ${Error.message}`;
+        }else{
+            return `An error occured: ${Error}`
+        }
+    }
+}
+
+const myArray:number[] = [1,2,3,4,5];
+
+const startRange = 1;
+const endRange = 10;
+const result2 = detectRange(myArray,startRange,endRange);
+
+console.log(result2);
