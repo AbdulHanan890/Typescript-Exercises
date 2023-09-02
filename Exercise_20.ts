360b3994edb1bce92ff391a8c8edc153d3cd78ee
AbdulHanan890 <143878161+AbdulHanan890@users.noreply.github.com>
2023-09-02 05:13:33 -0700

    simple object

---
+++ Exercise_20.ts
@@ -0,0 +1,14 @@
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items//
+
+let person = {
+
+    firstname: `Ali`,
+    lastname: `Ammar`,
+    Age: 25 ,
+    isstudent: false
+};
+
+console.log(person.firstname);
+console.log(person.lastname);
+console.log(person.Age);
+console.log(person.isstudent);
