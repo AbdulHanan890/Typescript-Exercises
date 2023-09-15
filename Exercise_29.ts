fc597adb3450d9e12d95a7da70a6d954e32097d3
AbdulHanan890 <143878161+AbdulHanan890@users.noreply.github.com>
2023-09-15 14:55:14 -0700

    loop through an array

--- Exercise_29.ts
+++ Exercise_29.ts
@@ -2,9 +2,6 @@
 //to each user after they log in to a website. Loop through the array, and print a greeting to each user:
 // • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
 // • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
-// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
-// • If the list is empty, print the message We need to find some users!
-// • Remove all of the usernames from your array, and make sure the correct message is printed
 
 
 let username: string[] = ["admin","Hanan","Ammar","Ahsan","Hassan"];
